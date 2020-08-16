614743e4837312b8d048a1e803f47002e0709004
naman10110105 <namanvashishth12@gmail.com>
2020-08-16 16:08:38 +0530

    test

--- dictionary.py
+++ dictionary.py
@@ -18,7 +18,7 @@ def dictionary(word):
         if suggestion == 'y':
             return data[get_close_matches(word, data.keys(), cutoff=0.8)[0]]
         else:
-            return "The word does not existssss."
+            return "The word does not exist."
     else:
         return "Please enter a valid word."
 
